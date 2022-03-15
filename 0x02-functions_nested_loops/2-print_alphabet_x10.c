9187716b52b036fc811cd785cd06a86648235ebd
kalinaD1924 <kaliyyeek@gmail.com>
2022-03-15 18:33:32 +0100

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alhpabet_x10 - repeats the print_alphabet 10 times.
+ * Return: Nothing.
+ */
+void print_alphabet_x10
+{ 
+	int i;
+	char c;
+	for (1 - 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++ )
+		{ 
+		    _putchar(c);
+		 }
+		_putchar('\n');
+	}
+
+}
+	
